7b7c7d52606a647650c0c7ad01f7e7f52aa4b675
Jed Rendo Magracia <jedmagracia@gmail.com>
2024-01-02 17:51:01 -0500

    feat: Initialized MediaInterface

---
+++ src/mediagrapher/media/media_interface.py
@@ -0,0 +1,111 @@
+"""
+Media Class
+"""
+
+import abc
+import numpy as np
+
+
+class MediaInterface(metaclass=abc.ABCMeta):
+    """
+    Represents a media object.
+    """
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'load_data_source') and
+                callable(subclass.load_data_source) and
+                hasattr(subclass, 'extract_text') and
+                callable(subclass.extract_text) or
+                NotImplemented)
+
+    @abc.abstractmethod
+    def __init__(self) -> None:
+        """
+        Initializes the Media object.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Media object.
+
+        Returns:
+            str: The string representation of the Media object.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def to_numpy_array(self) -> np.ndarray:
+        """
+        Converts the media object to a NumPy array.
+
+        Returns:
+            np.ndarray: The media object as a NumPy array.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_canny(self, low_threshold, high_threshold) -> np.ndarray:
+        """
+        Apply Canny edge detection to the image.
+
+        Args:
+            low_threshold (int): The lower threshold value for the hysteresis procedure.
+            high_threshold (int): The higher threshold value for the hysteresis procedure.
+
+        Returns:
+            np.ndarray: The resulting image after applying Canny edge detection.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_sobel(self) -> np.ndarray:
+        """
+        Apply Sobel edge detection to the image.
+
+        Returns:
+            np.ndarray: The resulting image after applying Sobel edge detection.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def resize_resolution(self, width: int, height: int) -> None:
+        """
+        Resizes the media object to the specified resolution.
+
+        Args:
+            width (int): The new width of the media object.
+            height (int): The new height of the media object.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def resize_scale(self, scale: float) -> None:
+        """
+        Resizes the media object by the specified scale factor.
+
+        Args:
+            scale (float): The scale factor to resize the media object.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rotate(self, angle: float) -> None:
+        """
+        Rotates the media object by the specified angle.
+
+        Args:
+            angle (float): The angle in degrees to rotate the media object.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def change_format(self, new_format: str) -> None:
+        """
+        Changes the format of the media object to the specified format.
+
+        Args:
+            new_format (str): The new format of the media object.
+        """
+        raise NotImplementedError
