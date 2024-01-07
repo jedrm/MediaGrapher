5e0d9af5927a8989ec2bad6422934f63b6710be6
Jed Rendo Magracia <jedmagracia@gmail.com>
2024-01-07 13:17:11 -0500

    Updated image.py

--- src/mediagrapher/media/image.py
+++ src/mediagrapher/media/image.py
@@ -3,7 +3,7 @@ Image Class
 """
 
 import numpy as np
-import requests
+import cv2
 from PIL import Image
 from .media import Media
 
@@ -18,14 +18,8 @@ class ImageMedia(Media):
         Initializes the Image object.
         """
         super().__init__(url, filename)
-        response = requests.head(url, allow_redirects=True, timeout=10)
-
-        if response.status_code == 200:
-            with open(f"frames/{filename}", 'wb') as file:
-                file.write(response.content)
-
-        image = Image.open(f"frames/{filename}")
-        self.image_array = np.array(image)
+        self.image = Image.open(f"frames/{filename}")
+        self.resolution = self.image.size
 
     def __str__(self) -> str:
         """
@@ -34,7 +28,7 @@ class ImageMedia(Media):
         Returns:
             str: The string representation of the Image object.
         """
-        return f"ImageMedia(url={self.url}, filename={self.filename})"
+        return f"ImageMedia(url={self.url}, filename={self.filename}), resolution={self.resolution}"
 
     def to_numpy_array(self) -> np.ndarray:
         """
@@ -43,7 +37,7 @@ class ImageMedia(Media):
         Returns:
             np.ndarray: The image object as a NumPy array.
         """
-        return self.image_array
+        return np.array(self.image)
 
     def get_canny(self, low_threshold, high_threshold) -> np.ndarray:
         """
@@ -56,7 +50,7 @@ class ImageMedia(Media):
         Returns:
             np.ndarray: The resulting image after applying Canny edge detection.
         """
-        raise NotImplementedError
+        return cv2.Canny(self.to_numpy_array(), low_threshold, high_threshold)
 
     def get_sobel(self) -> np.ndarray:
         """
@@ -65,7 +59,24 @@ class ImageMedia(Media):
         Returns:
             np.ndarray: The resulting image after applying Sobel edge detection.
         """
-        raise NotImplementedError
+        scale = 1
+        delta = 0
+        ddepth = cv2.CV_16S
+
+        src = cv2.imread(self.filename, cv2.IMREAD_COLOR)
+        src = cv2.GaussianBlur(src, (3, 3), 0)
+        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+        grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale,
+                           delta=delta, borderType=cv2.BORDER_DEFAULT)
+        grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale,
+                           delta=delta, borderType=cv2.BORDER_DEFAULT)
+
+        abs_grad_x = cv2.convertScaleAbs(grad_x)
+        abs_grad_y = cv2.convertScaleAbs(grad_y)
+
+        grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+        return grad
 
     def resize_resolution(self, width: int, height: int) -> None:
         """
@@ -75,7 +86,7 @@ class ImageMedia(Media):
             width (int): The new width of the image object.
             height (int): The new height of the image object.
         """
-        raise NotImplementedError
+        self.image = self.image.resize((width, height))
 
     def resize_scale(self, scale: float) -> None:
         """
@@ -84,7 +95,8 @@ class ImageMedia(Media):
         Args:
             scale (float): The scale factor to resize the image object.
         """
-        raise NotImplementedError
+        self.image = self.image.resize(
+            (int(self.resolution[0] * scale), int(self.resolution[1] * scale)))
 
     def rotate(self, angle: float) -> None:
         """
@@ -93,7 +105,7 @@ class ImageMedia(Media):
         Args:
             angle (float): The angle in degrees to rotate the image object.
         """
-        raise NotImplementedError
+        self.image = self.image.rotate(angle)
 
     def change_format(self, new_format: str) -> None:
         """
