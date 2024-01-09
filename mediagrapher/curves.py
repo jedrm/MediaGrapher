2adabd87ccde3ef277c5a0a37586c1209b099b58
Jed Rendo Magracia <jedmagracia@gmail.com>
2024-01-09 16:36:45 -0500

    feat: Created curves.py and the Curves class

---
+++ mediagrapher/curves.py
@@ -0,0 +1,159 @@
+"""
+Curves class
+"""
+
+from typing import List, Tuple
+import numpy as np
+import potrace
+from .media.media import Media
+
+
+class Curves:
+    """
+    A class representing curves in a media.
+
+    Methods:
+    - get_segments: Returns the segments of the curves.
+    - get_bezier_curves: Returns the Bezier curves of the curves.
+    - get_latex: Returns the LaTeX representation of the curves.
+    - export_latex: Exports the curves to a LaTeX file.
+    """
+
+    def __init__(self, media: Media, algorithm: str = "Canny"):
+        """
+        Initializes the Curves object.
+
+        Args:
+            media (Media): The media object.
+        """
+        match algorithm:
+            case "Canny":
+                self.media = media.get_canny()
+            case "Sobel":
+                self.media = media.get_sobel()
+            case _:
+                raise ValueError("Invalid algorithm.")
+
+        for i, _ in enumerate(self.media):
+            self.media[i][self.media[i] > 1] = 1
+
+        self.bitmap = potrace.Bitmap(self.media)
+        self.path = self.bitmap.trace()
+
+    def get_segments(self) -> List[List[Tuple[float, float]]]:
+        """
+        Returns a list of segments in the path, where each segment is represented as a list of integers.
+
+        Each segment consists of a start point, an end point, and control points (if applicable).
+        If the segment is a corner, it will have a single control point 'c'.
+        If the segment is a curve, it will have two control points 'c1' and 'c2'.
+
+        Returns:
+            List[List[float]]: A list of segments, where each segment is represented as a list of integers.
+        """
+        segments = []
+        for curve in self.path:
+            start_point = curve.start_point
+            for segment in curve:
+                end_point = segment.end_point
+                if segment.is_corner:
+                    c = segment.c
+                    segments.append([start_point, end_point, c])
+                else:
+                    c1 = segment.c1
+                    c2 = segment.c2
+                    segments.append([start_point, end_point, c1, c2])
+        return segments
+
+    def get_coordinates(self, linspace: int = 100) -> List[List[List[float]]]:
+        """
+        Returns the coordinates of the curves in the path.
+
+        Args:
+            linspace (int): The number of points to generate along each curve segment. Default is 100.
+
+        Returns:
+            List[List[List[float], List[float]]]: A list of coordinates for each curve segment in the path.
+                Each coordinate is represented as a list of x and y values.
+
+        Raises:
+            None.
+        """
+        coordinates = []
+
+        for curve in self.path:
+            start_x, start_y = curve.start_point
+            start_x, start_y = [start_x] * linspace, [start_y] * linspace
+
+            for segment in curve:
+                if segment.is_corner:
+                    c_x, c_y = segment.c
+                    end_x, end_y = segment.end_point
+
+                    c_x, c_y = [c_x] * linspace, [c_y] * linspace
+                    end_x, end_y = [end_x] * linspace, [end_y] * linspace
+
+                    t = np.linspace(0, 1, linspace)
+
+                    first_line_x = list(map(
+                        self.linear_bezier_curve, start_x, c_x, t))
+                    first_line_y = list(map(
+                        self.linear_bezier_curve, start_y, c_y, t))
+
+                    second_line_x = list(map(
+                        self.linear_bezier_curve, c_x, end_x, t))
+                    second_line_y = list(map(
+                        self.linear_bezier_curve, c_y, end_y, t))
+
+                    coordinates.append([first_line_x, first_line_y])
+                    coordinates.append([second_line_x, second_line_y])
+
+                else:
+                    c1_x, c1_y = segment.c1
+                    c2_x, c2_y = segment.c2
+                    end_x, end_y = segment.end_point
+
+                    c1_x, c1_y = [c1_x] * linspace, [c1_y] * linspace
+                    c2_x, c2_y = [c2_x] * linspace, [c2_y] * linspace
+                    end_x, end_y = [end_x] * linspace, [end_y] * linspace
+
+                    t = np.linspace(0, 1, linspace)
+
+                    curve_x = list(map(self.cubic_bezier_curve,
+                                       start_x, end_x, c1_x, c2_x, t))
+                    curve_y = list(map(self.cubic_bezier_curve,
+                                       start_y, end_y, c1_y, c2_y, t))
+
+                    coordinates.append([curve_x, curve_y])
+
+        return coordinates
+
+    def linear_bezier_curve(self, start_point: int, end_point: int, t: float) -> float:
+        """
+        Returns the linear Bezier curve.
+
+        Args:
+            start_point (int): The start point.
+            end_point (int): The end point.
+            t (float): The t value.
+
+        Returns:
+            int: The linear Bezier curve.
+        """
+        return (1 - t) * start_point + t * end_point
+
+    def cubic_bezier_curve(self, start_point: int, end_point: int, c1: int, c2: int, t: float) -> float:
+        """
+        Returns the cubic Bezier curve.
+
+        Args:
+            start_point (int): The start point.
+            end_point (int): The end point.
+            c1 (int): The first control point.
+            c2 (int): The second control point.
+            t (float): The t value.
+
+        Returns:
+            int: The cubic Bezier curve.
+        """
+        return (1 - t) ** 3 * start_point + 3 * (1 - t) ** 2 * t * c1 + 3 * (1 - t) * t ** 2 * c2 + t ** 3 * end_point
